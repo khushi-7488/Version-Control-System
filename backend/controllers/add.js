c4e2a0c0edbb751224230d81acba14d6b58252bc
khushi-7488 <khushiitiwari88@gmail.com>
2025-11-08 20:23:25 +0530

    completed controllers

--- backend/controllers/add.js
+++ backend/controllers/add.js
@@ -5,7 +5,7 @@ async function addRepo(filePath) {
     const repoPath = path.resolve(process.cwd(), ".apnaGit");
     const staggingPath = path.join(repoPath, "staging");
     try {
-        await fs.mkdir(staggingPath, { recurseve: true })
+        await fs.mkdir(staggingPath, { recursive: true })
         const fileName = path.basename(filePath);
         await fs.copyFile(filePath, path.join(staggingPath, fileName))
         console.log(`File ${fileName} added to the staging area!`)
--- backend/index.js
+++ backend/index.js
@@ -3,41 +3,52 @@ const { hideBin } = require("yargs/helpers");
 const { initRepo } = require("./controllers/init.js");
 const { addRepo } = require("./controllers/add.js");
 const { commitRepo } = require("./controllers/commit.js");
-const { pushRepo } = require("./controllers/push.js")
-const { pullRepo } = require("./controllers/pull.js")
-const { revertRepo } = require("./controllers/revert.js")
+const { pushRepo } = require("./controllers/push.js");
+const { pullRepo } = require("./controllers/pull.js");
+const { revertRepo } = require("./controllers/revert.js");
 
 yargs(hideBin(process.argv))
-    .command("init", "Initialise a new repository", {}, initRepo)
-    .command(
-        "add <file>",
-        "add a new repository",
-        (yargs) => {
-            yargs.positional("file", {
-                describe: "file to add to the staging area",
-                type: "String",
-            });
-        },
-        (argv) => {
-            addRepo(argv.file);
-        }
-    )
-    .command("commit <message>", "commit a new repository", (yargs) => {
-        yargs.positional("message", {
-            describe: "message to commit to the staging area",
-            type: "String",
-
-        })
-    }, (argv) => {
-        commitRepo(argv.message);
-    })
-    .command("push", "push a new repository", {}, pushRepo)
-    .command("pull", "pull a new repository", {}, pullRepo)
-    .command("revert <commitID>", "revert a new repository", (yargs) => {
-        yargs.positional("commitID", {
-            describe: "commit ID to revert to",
-            type: "string"
-        })
-    }, revertRepo)
-    .demandCommand(1, "you need atleast one command")
-    .help().argv;
+  .command("init", "Initialise a new repository", {}, initRepo)
+  .command(
+    "add <file>",
+    "add a new repository",
+    (yargs) => {
+      yargs.positional("file", {
+        describe: "file to add to the staging area",
+        type: "String",
+      });
+    },
+    (argv) => {
+      addRepo(argv.file);
+    }
+  )
+  .command(
+    "commit <message>",
+    "commit a new repository",
+    (yargs) => {
+      yargs.positional("message", {
+        describe: "message to commit to the staging area",
+        type: "String",
+      });
+    },
+    (argv) => {
+      commitRepo(argv.message);
+    }
+  )
+  .command("push", "push a new repository", {}, pushRepo)
+  .command("pull", "pull a new repository", {}, pullRepo)
+  .command(
+    "revert <commitID>",
+    "revert a new repository",
+    (yargs) => {
+      yargs.positional("commitID", {
+        describe: "commit ID to revert to",
+        type: "string",
+      });
+    },
+    (argv) => {
+      revertRepo(argv.commitID);
+    }
+  )
+  .demandCommand(1, "you need atleast one command")
+  .help().argv;

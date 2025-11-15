a0c3cd42c934238b9122ad0aad63e8ec4b21aa30
khushi-7488 <khushiitiwari88@gmail.com>
2025-11-15 23:53:10 +0530

    building signup

---
+++ frontend/src/components/auth/Signup.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Signup.css";
+
+const Signup = () => {
+  console.log("Signup component is rendering!");
+  return (
+    <div className="main-container">
+      <div>
+        <h5>helloooooo</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;

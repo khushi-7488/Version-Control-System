ffdd4464eaacd3d538585b254775ab4ddbf39edc
khushi-7488 <khushiitiwari88@gmail.com>
2025-11-16 14:53:15 +0530

    build Login

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,5 +1,95 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./auth.css";
+import logo from "../../assets/github-mark-white.svg";
+import { Link } from "react-router-dom";
+import { useAuth } from "../../authContext";
 
-const Login = () => {};
+const Login = () => {
+  useEffect(() => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("userId");
+    setCurrentUser(null);
+  });
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const { currentUser, setCurrentUser } = useAuth();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      const res = await axios.post("http://localhost:8080/auth", {
+        email: email,
+        password: password,
+      });
+
+      localStorage.setItem("token", res.data.token);
+      localStorage.setItem("userId", res.data.userId);
+
+      setCurrentUser(res.data.userId);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+      alert("login failed");
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <div className="main-container">
+        <img className="mb-2 mt-5 logo" src={logo} alt="" />
+        <h2 className="fs-4 p-4">Sign Up to GitHub</h2>
+        <div className="heading">
+          <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              class="form-control"
+              id="exampleFormControlInput1"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">
+              Password
+            </label>
+            <input
+              type="email"
+              class="form-control"
+              id="exampleFormControlInput1"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div class="d-grid gap-2">
+            <button
+              onClick={handleLogin}
+              class="btn btn-success mt-3"
+              type="button"
+              disabled={loading}
+            >
+              {loading ? "Loading..." : "Signup"}
+            </button>
+          </div>
+          <hr />
+          <div className="text-center">
+            <p>
+              Already have an account? <Link to="/signup">Sign in</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Login;
